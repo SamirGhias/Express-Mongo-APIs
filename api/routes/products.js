d320859b38880fc33b401ef8b52d48cd0e4b32df
SamirGhias <samir.ghias99@gmail.com>
2024-06-11 23:36:19 -0400

    pushing basic crud with mongodb

---
+++ api/routes/products.js
@@ -0,0 +1,113 @@
+const express = require('express');
+const router = express.Router();
+
+const Product = require('../models/product');
+
+router.get('/', (req, res, next) => {
+  Product.find()
+    .then((products) => {
+      res.status(200).json({
+        message: 'Handling GET requests to /products',
+        products: products,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+      });
+    });
+});
+router.post('/', (req, res, next) => {
+  const product = new Product({
+    name: req.body.name,
+    price: req.body.price,
+  });
+
+  product
+    .save()
+    .then((result) => {
+      console.log(result);
+      res.status(201).json({
+        message: 'Handling POST requests to /products',
+        createdProduct: product,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+});
+
+router.get('/:productId', (req, res, next) => {
+  const id = req.params.productId;
+  const product = Product.findById(id)
+    .then((doc) => {
+      console.log(doc);
+      // console.log(product);
+      if (doc) {
+        res.status(200).json(doc);
+      } else {
+        res.status(404).json({ message: 'No product found' });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
+  // console.log('Product found!: ', product._doc);
+  // if (id === 'special') {
+  //   res.status(200).json({
+  //     message: 'You discovered the secret ID',
+  //     id: id,
+  //   });
+  // } else {
+  //   res.status(200).json({
+  //     message: 'You passed an ID',
+  //     id: id,
+  //   });
+  // }
+});
+
+router.patch('/:productId', (req, res, next) => {
+  const id = req.params.productId;
+  const updateOps = {};
+  console.log(req.body);
+  for (const ops of req.body) {
+    console.log('inside', ops);
+    updateOps[ops.propName] = ops.value;
+  }
+  console.log('UPDATE THE FOLLOWING: ', updateOps);
+  Product.updateOne({ _id: id }, { $set: updateOps })
+    .then((result) => {
+      console.log(result);
+      res.status(200).json({
+        message: 'Updated product',
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
+});
+
+router.delete('/:productId', (req, res, next) => {
+  const id = req.params.productId;
+
+  Product.deleteOne({ _id: id })
+    .then((doc) => {
+      console.log(doc);
+      res.status(200).json(doc);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+      });
+    });
+  // res.status(200).json({
+  //   message: 'Deleted product',
+  // });
+});
+
+module.exports = router;
---
+++ app.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const mongoose = require('mongoose');
+const username = encodeURIComponent(process.env.MONGO_USER);
+const password = encodeURIComponent(process.env.MONGO_PASSWORD);
+const db_name = encodeURIComponent(process.env.MONGO_DB);
+const uri = `mongodb+srv://${username}:${password}@cluster0.jcetwuv.mongodb.net/${db_name}?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose.connect(uri);
+// .then(() => {
+//   app.listen(3000);
+// })
+// .catch((err) => {
+//   console.log(err);
+// });
+
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  // console.log('INSIDE');
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+});
+
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
+
+module.exports = app;
